20f30971ed80dd5215e90d1805dd083e1c74ae98
Radostina Cvetkova <radost.tsvetkova@gmail.com>
2017-11-25 09:20:29 +0200

    Adding first two lab exercises

---
+++ StringsAndRegEx/CountSubstringOccurrences/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSubstringOccurrences
+{
+    class CountSubstringOccurrences
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string targetStr = Console.ReadLine();
+
+            int occurrencesCount = 0;
+            int positionOfTargetStr = -1;
+
+            while (true)
+            {
+                positionOfTargetStr = input.IndexOf(targetStr, positionOfTargetStr + 1, StringComparison.OrdinalIgnoreCase);
+
+                if (positionOfTargetStr == -1)
+                {
+                    break;
+                }
+                occurrencesCount++;
+            }
+
+            Console.WriteLine(occurrencesCount);
+        }
+    }
+}
---
+++ StringsAndRegEx/ReverseString/ReverseString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            var wordReversed = word.ToCharArray().Reverse();
+
+            Console.WriteLine(string.Concat(wordReversed));
+        }
+    }
+}
