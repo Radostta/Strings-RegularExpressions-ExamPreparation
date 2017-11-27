634887d569efc7cdbbd14efefa334114b9c64c41
Radostina Cvetkova <radost.tsvetkova@gmail.com>
2017-11-27 07:23:38 +0200

    Adding "Trainegram"

---
+++ Trainegram/Trainegram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Trainegram
+{
+    class Trainegram
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^(<\[[^a-zA-Z0-9]*?\]\.)(\.\[[0-9A-Za-z]*?\]\.)*?$";
+
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+
+            while (input != "Traincode!")
+            {
+                if (regex.IsMatch(input))
+                {
+                    Console.WriteLine(input);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
