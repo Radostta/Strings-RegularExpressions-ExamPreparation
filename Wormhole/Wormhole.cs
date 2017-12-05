fad740ba9d7132a53a0304639bb425fd546cc071
Radostina Cvetkova <radost.tsvetkova@gmail.com>
2017-12-05 20:35:27 +0200

    Adding Wormhole.cs

---
+++ Wormhole/Wormhole.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Wormhole
+{
+    class Wormhole
+    {
+        static void Main(string[] args)
+        {
+            var wormholes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int steps = 0;
+
+            for (int i = 0; i < wormholes.Length; i++)
+            {
+                if (wormholes[i] == 0)
+                {
+                    steps++;
+                    continue;
+                }
+                
+                int index = wormholes[i];                
+                wormholes[i] = 0;
+                i = index - 1;
+            }
+
+            Console.WriteLine(steps); ;
+        }
+    }
+}
