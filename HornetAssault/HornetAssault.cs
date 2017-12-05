362189e6cb83d1e028060080e5a8e45072834abb
Radostina Cvetkova <radost.tsvetkova@gmail.com>
2017-12-05 13:05:30 +0200

    Adding HornetAssault.cs and NetherRealms.cs

---
+++ HornetAssault/HornetAssault.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HornetAssault
+{
+    class HornetAssault
+    {
+        static void Main(string[] args)
+        {
+            var bees = Console.ReadLine().Split(' ').Select(BigInteger.Parse).ToList();
+            var hornets = Console.ReadLine().Split(' ').Select(BigInteger.Parse).ToList();
+            BigInteger hornetPower = 0;
+
+            //hornetPower = hornets.Sum();
+
+            for (int h = 0; h < hornets.Count; h++)
+            {
+                hornetPower += hornets[h];
+            }
+
+            for (int i = 0; i < bees.Count; i++)
+            {
+                if (bees[i] < hornetPower)
+                {
+                    bees[i] -= hornetPower;
+                }
+                else
+                {
+                    bees[i] -= hornetPower;
+
+                    if (hornets.Any())
+                    {
+                        hornetPower -= hornets[0];
+                        hornets.RemoveAt(0);
+                    }
+                }                
+            }
+
+            if (bees.Any(x => x > 0))
+            {
+                Console.WriteLine(string.Join(" ", bees.Where(x => x > 0)));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", hornets));
+            }
+        }
+    }
+}
---
+++ NetherRealms/NetherRealms.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NetherRealms
+{
+    class NetherRealms
+    {
+        static void Main(string[] args)
+        {
+            Regex healthRegex = new Regex(@"[^0-9\+\-\*\/\.\s\,]");
+            
+            Regex damageRegex = new Regex(@"[\-\+]?[\d]+(?:[\.]*[\d]+|[\d]*)");
+
+            Regex damageOperatorsRegex = new Regex(@"\*?\/?");
+
+            var participantsList = new List<string>();
+
+            var participantsNames = Console.ReadLine().Split(new[] { ',', ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            for (int i = 0; i < participantsNames.Count; i++)
+            {
+                string demonName = participantsNames[i];
+
+                //Health:
+                MatchCollection healthMatches = healthRegex.Matches(demonName);
+
+                string name = "";
+                foreach (Match ch in healthMatches)
+                {
+                    name += ch.ToString();
+                }
+
+                long health = 0;
+                for (int l = 0; l < name.Length; l++)
+                {
+                    health += (int)name[l];
+                }
+
+                //Damage:
+                MatchCollection damageMatches = damageRegex.Matches(demonName);
+                decimal damage = 0.0m;                
+
+                foreach (Match number in damageMatches)
+                {
+                    if (!number.ToString().Equals(string.Empty))
+                    {                        
+                        damage += 0.0m;
+                    }
+
+                    damage += decimal.Parse(number.ToString());
+                }
+                
+                MatchCollection damageOperatorsMatches = damageOperatorsRegex.Matches(demonName.ToString());
+                var operators = new List<string>();
+
+                foreach (Match op in damageOperatorsMatches)
+                {
+                    if (!op.ToString().Equals(string.Empty))
+                    {
+                        operators.Add(op.ToString());
+                    }                        
+                }
+
+                if (operators.Any())
+                {
+                    foreach (var op in operators)
+                    {
+                        if (op == "*")
+                        {
+                            damage *= 2.0m;
+                        }
+                        else if (op == "/")
+                        {
+                            damage /= 2.0m;
+                        }
+                    }
+                }
+
+                string dragonInfo = $"{demonName} - {health} health, {damage:f2} damage";
+                participantsList.Add(dragonInfo);
+            }
+
+            //Printing:
+            foreach (var participant in participantsList.OrderBy(x => x))
+            {
+                Console.WriteLine(participant);
+            }
+
+        }
+    }
+}
