f88c42b4b2d354ea4ad71bcc3550ff0490eacf75
Radostina Cvetkova <radost.tsvetkova@gmail.com>
2017-12-05 15:55:05 +0200

    Adding RageQuit.cs and Spyfer.cs

---
+++ RageQuit/RageQuit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RageQuit
+{
+    class RageQuit
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"([^0-9]+)([0-9]+)");
+            string input = Console.ReadLine();
+
+            StringBuilder message = new StringBuilder();
+
+            MatchCollection inputParts = regex.Matches(input);
+
+            foreach (Match part in inputParts)
+            {
+                string text = part.Groups[1].Value.ToUpper();
+                int repeat = int.Parse(part.Groups[2].Value);                
+
+                for (int i = 0; i < repeat; i++)
+                {
+                    message.Append(text);
+                }
+            }
+            
+            int uniqueSymbolsCount = message.ToString().Distinct().Count();            
+
+            Console.WriteLine($"Unique symbols used: {uniqueSymbolsCount}");
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Spyfer/Spyfer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spyfer
+{
+    class Spyfer
+    {
+        static void Main(string[] args)
+        {
+            var coordinates = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            for (int i = 0; i < coordinates.Count; i++)
+            {
+                int currentNumber = coordinates[i];
+
+                if (i != 0 && i != coordinates.Count - 1)
+                {
+                    int sumOfNeighbours = coordinates[i - 1] + coordinates[i + 1];
+
+                    if (currentNumber == sumOfNeighbours)
+                    {
+                        coordinates.RemoveAt(i + 1);
+                        coordinates.RemoveAt(i - 1);
+                        i = 0;
+                    }
+                }
+                else if (i == 0 && currentNumber == coordinates[i + 1])
+                {
+                    coordinates.RemoveAt(i + 1);
+                    i = 0;
+                }
+                else if (i == coordinates.Count - 1 && currentNumber == coordinates[i - 1])
+                {
+                    coordinates.RemoveAt(i - 1);
+                    i = 0;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", coordinates));
+        }
+    }
+}
